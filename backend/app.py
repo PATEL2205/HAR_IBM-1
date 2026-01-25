dc782d98df93ae5a6be8334622755e3e519cc83d
Dhruv Jethwani <dhruvjethwani56@gmail.com>
2026-01-25 18:06:02 +0530

    register backend logic made

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify, request, redirect, render_template
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__)
 # This line tells Flask to allow requests from your React app
 CORS(app) 
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    #name, email and password columns
+
+    def check_user(email):
+        #DB query for searching for user in the database.
+        pass #remove this line when code login written
 
 @app.route('/api/test')
 def get_activity():
@@ -24,6 +35,23 @@ def login():
         "token": "your-auth-token-here"
     }), 200
 
+@app.route('/api/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    name = data.get('fullName')
+    email = data.get('email')
+    password = data.get('password')
+    confirm_pass = data.get('confirmPassword')
+
+    if User.check_user(email):
+        return jsonify({"error": "Email already registered"}), 400
+    if password != confirm_pass:
+        return jsonify({"message": "Passwords don't match"}), 400
+    
+    new_user = User(name=name, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created successfully"}), 201
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
